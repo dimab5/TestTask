60f47e39c9f83af511ac532320e75e4cc6b8c1fa
dimab5 <113174105+dimab5@users.noreply.github.com>
2024-02-12 23:22:29 +0300

    Add files via upload

---
+++ Task1/Request_2.sql
@@ -0,0 +1,15 @@
+WITH RECURSIVE ManagerChain AS (
+    SELECT ID, CHIEF_ID, 1 AS ChainLength
+    FROM EMPLOYEE
+    WHERE CHIEF_ID IS NULL
+
+    UNION ALL
+
+    SELECT e.ID, e.CHIEF_ID, mc.ChainLength + 1
+    FROM EMPLOYEE e
+    JOIN ManagerChain mc 
+    ON e.CHIEF_ID = mc.ID
+)
+
+SELECT MAX(ChainLength) AS MaxChainLength
+FROM ManagerChain;
